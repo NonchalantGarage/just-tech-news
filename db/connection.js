249ae49213456a1e660c369bc4d10e215cff3031
Curtis <hong.curtis@gmail.com>
2022-01-23 20:59:32 -0800

    added connectection to db and secured my password with dotenv npm package

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+
+require("dotenv").config();
+
+const sequealize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3006,
+  }
+);
+
+module.exports = sequealize;
